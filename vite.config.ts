5a1afd9afa07e2005da24cf8ae8a623922e77ce4
IK Mendoza <iken.mndz@gmail.com>
2025-08-04 12:17:30 +0800

    config updt

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,10 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@lib": path.resolve(__dirname, "./src/lib"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@layout": path.resolve(__dirname, "./src/layout"),
     },
   },
 })
