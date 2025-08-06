8ab5e4f77071c8a4dfefef88f467cb92ae65babb
Ian Kenneth <ian.mendoza@reliancehealth.com.ph>
2025-08-06 15:00:50 +0800

    fix

--- src/pages/customer/products/ProductsDetailPage.tsx
+++ src/pages/customer/products/ProductsDetailPage.tsx
@@ -9,30 +9,43 @@ const ProductsDetailPage = () => {
         <div style={{ height: "700px", width: "700px" }}>
           <img src="" alt="" style={{ height: "100%", width: "100%" }} />
         </div>
-        <div className="p-2 flex flex-col justify-center flex-grow flex-shrink self-auto" 
+        <div className="p-2 flex flex-col justify-center flex-grow flex-shrink self-auto gap-5" 
           style={{ 
-            height: "700px", width: "700px" 
+            height: "700px", width: "700px",
           }}
         >
           <div>
-            <div>
-              <div className="d-flex flex-row flex-wrap mb-2">
-                <span></span>
-              </div>
-              <h1 className="text-6xl">KAPITON POLO</h1>
+            <div className="d-flex flex-row flex-wrap mb-2">
+              <span></span>
             </div>
-            <div className="flex flex-row justify-start align-center gap-3">
-              <div className="flex flex-row justify-start align-center gap3">
-                <div>
-                  <img src="" alt="" style={{ width: "30px"}} />
-                </div>
-                <div>
-                  <span>Kapiton</span>
-                </div>
+            <h1 className="text-6xl">KAPITON POLO</h1>
+          </div>
+          <div className="flex flex-row justify-start align-center gap-3">
+            <div className="flex flex-row justify-start align-center gap3">
+              <div>
+                <img src="" alt="" style={{ width: "30px"}} />
               </div>
-              <div>No Reviews</div>
+              <div>
+                <span>Kapiton</span>
+              </div>
             </div>
-            <div></div>
+            <div>No Reviews</div>
+          </div>
+          <div className="flex flex-row justify-start align-center gap-3">
+            <span className="text-xl">P 999</span>
+          </div>
+          <div className="description"></div>
+          <div className="flex flex-col justify-start align-center gap-4">
+            <div className="flex flex-row align-center gap-5">
+              <div>
+                <span>Variation1</span>
+              </div>
+              <div>
+                <span>Variation2</span>
+              </div>
+            </div>
+            <div><span>Quantity</span><span>Stock</span></div>
+            <div><span>Button</span></div>
           </div>
         </div>
       </div>
