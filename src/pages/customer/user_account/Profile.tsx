241677f9db9404eddaf1fb2fdfbf310f9e3d3a21
IK Mendoza <iken.mndz@gmail.com>
2025-08-25 18:56:09 +0800

    Profile update

--- src/pages/customer/user_account/Profile.tsx
+++ src/pages/customer/user_account/Profile.tsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
-import { MDTextField, Select, TextField } from "@components";
+import { MDTextField, Select, useMessageDialog, TextField } from "@components";
 import { useAppContext } from "@context/AppContext";
 import { saveCustomerProfileInfo } from "@api"
 import { useMutation, useQuery } from "@tanstack/react-query";
 
 export default () => {
   const { state, setState } = useAppContext();
+  const { showMessage } = useMessageDialog();
 
   const customerProfileInfoMutation = useMutation({
     mutationFn: (formData) => saveCustomerProfileInfo(state.token, formData),
     onSuccess: (resp) => {
+      showMessage({
+        open: true,
+        message: resp.message,
+        type: "success",
+        title: "Profile update."
+      })
       let userState = state.user
       userState = {...userState, ...form};
       setState({...state, 
@@ -17,9 +24,22 @@ export default () => {
       })
       localStorage.setItem('user', JSON.stringify(userState))
       setEditing(false)
-    }
+    },
+    onError: (error) => {
+      setSaving(false)
+      showMessage({
+        open: true,
+        message: error.message,
+        type: "error",
+        title: "Profile update."
+      })
+      if (error.errors != undefined) {
+        setFormErrors(error.errors)
+      }
+    },
   });
 
+  const [saving, setSaving] = useState(false)
   const [editing, setEditing] = useState(false);
   const [form, setForm] = useState({
     first_name: state.user.first_name || "",
@@ -32,6 +52,16 @@ export default () => {
     country: state.user.country || "Philippines",
     pincode: state.user.pincode || ""
   });
+  const [formErrors, setFormErrors] = useState({
+    first_name: "",
+    last_name: "",
+    mobile: "",
+    email: "",
+    address: "",
+    city: "",
+    state: "",
+    pincode: ""
+  });
 
   const handleEdit = () => setEditing(true);
   const handleCancel = () => setEditing(false);
@@ -42,6 +72,7 @@ export default () => {
 
   const handleSave = (e) => {
     e.preventDefault();
+    setSaving(true)
     customerProfileInfoMutation.mutate(form);
   };
 
@@ -104,10 +135,16 @@ export default () => {
             </div>
             <div className="flex flex-wrap gap-4">
               <div>
-                <MDTextField label="First Name" value={form.first_name} onChange={e => handleChange('first_name', e.target.value)} />
+                <MDTextField label="First Name" value={form.first_name} 
+                  onChange={e => handleChange('first_name', e.target.value)} 
+                  err={formErrors.first_name}
+                />
               </div>
               <div>
-                <MDTextField label="Last Name" value={form.last_name} onChange={e => handleChange('last_name', e.target.value)} />
+                <MDTextField label="Last Name" value={form.last_name} 
+                  onChange={e => handleChange('last_name', e.target.value)}
+                  err={formErrors.last_name}
+                />
               </div>
             </div>
           </div>
@@ -117,10 +154,16 @@ export default () => {
             </div>
             <div className="flex flex-wrap gap-4">
               <div>
-                <MDTextField label="Mobile" value={form.mobile} onChange={e => handleChange('mobile', e.target.value)} />
+                <MDTextField label="Mobile" value={form.mobile} 
+                  onChange={e => handleChange('mobile', e.target.value)} 
+                  err={formErrors.mobile}
+                />
               </div>
               <div>
-                <MDTextField label="Email" value={form.email} onChange={e => handleChange('email', e.target.value)} />
+                <MDTextField label="Email" value={form.email} 
+                  onChange={e => handleChange('email', e.target.value)} 
+                  err={formErrors.email}
+                />
               </div>
             </div>
           </div>
@@ -130,24 +173,47 @@ export default () => {
             </div>
             <div className="w-100 mb-3 grid grid-cols-1 md:grid-cols-2 gap-4">
               <div>
-                <MDTextField label="Address" value={form.address} onChange={e => handleChange('address', e.target.value)} />
+                <MDTextField label="Address" value={form.address} 
+                  onChange={e => handleChange('address', e.target.value)} 
+                  err={formErrors.address}
+                />
               </div>
               <div>
-                <MDTextField label="City" value={form.city} onChange={e => handleChange('city', e.target.value)} />
+                <MDTextField label="City" value={form.city} 
+                  onChange={e => handleChange('city', e.target.value)} 
+                  err={formErrors.city}
+                />
               </div>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-3">
-              <Select label="Province" options={statesQuery.data} value={form.state} onChange={e => handleChange('state', e.target.value)} />
-              <Select label="Country" options={country} value={form.country} onChange={e => handleChange('country', e.target.value)} />
+              <Select label="Province" options={statesQuery.data} 
+                value={form.state} 
+                onChange={e => handleChange('state', e.target.value)}
+              />
+              <Select label="Country" options={country} 
+                value={form.country} 
+                onChange={e => handleChange('country', e.target.value)}
+              />
+              <div>
+                <span className="error-message text-red-500 text-sm">{formErrors.state}</span>
+              </div>
             </div>
             <div className="mb-3 grid grid-cols-1 md:grid-cols-3 gap-4">
               <div>
-                <MDTextField label="ZIP Code" value={form.pincode} onChange={e => handleChange('pincode', e.target.value)} />
+                <MDTextField label="ZIP Code" value={form.pincode} 
+                  onChange={e => handleChange('pincode', e.target.value)}
+                  err={formErrors.pincode}
+                />
               </div>
             </div>
           </div>
           <div className="flex gap-4 mt-6">
-            <button className="btn btn-primary bg-primary px-6 py-2 font-semibold rounded" onClick={handleSave}>Save</button>
+            <button className={[
+              "btn btn-primary bg-primary px-6 py-2 font-semibold rounded",
+              saving ? "bg-gray-300":""
+            ].join(" ")} disabled={saving} onClick={handleSave}>
+              {saving ? "Saving...":"Save"}
+            </button>
             <button className="btn btn-secondary px-6 py-2 font-semibold rounded" onClick={handleCancel}>Cancel</button>
           </div>
         </div>
